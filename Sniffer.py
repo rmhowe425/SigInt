9666bc70e9265f7125e56e4103f7860a3d6cecbd
rmhowe425 <45905457+rmhowe425@users.noreply.github.com>
2020-12-29 13:14:30 -0500

    Add files via upload

--- Sniffer.py
+++ Sniffer.py
@@ -18,7 +18,7 @@ class Sniffer:
         self.sniff = pcap(name = name, promisc = True, immediate = True, timeout_ms = 50)
         self.pkt = self.sniff.readpkts()
         self.size = len(self.pkt)
-        self.max_num = 100 # 99 threads created
+        self.max_num = 99
 
     '''
         Filter out and sort packets as either a client or access point.
@@ -34,15 +34,21 @@ class Sniffer:
                 self.cond.wait()
 
             # Done criteria met, kill thread.
-            if num > self.size:
+            if self.max_num > self.size:
+                if num == self.max_num:
+                    self.max_num -= 1
+                    self.flag = self.max_num
+                    self.cond.notify_all()
+                    self.lock.release()
+                    return None
+
+                self.flag = self.max_num
                 self.cond.notify_all()
                 self.lock.release()
-                print("Thread {} finished.\n".format(num))
-                return None
 
             # Work to do
             else:
-                pk = self.pkt[self.size - num]
+                pk = self.pkt[self.size - num - 1][1]
                 offset, rt_data = radiotap_parse(pk)
 
                 # Broadcast-type of packet (signifies an access point)
@@ -59,19 +65,21 @@ class Sniffer:
                     # Add new item to dictionary
                     self.ap_list[ssid] = AccessPoint(pwr, mac, ssid)
 
-            # Remove element from list, decrement list size
-            self.pkt.pop(self.size - num)
-            self.size -= 1
+                # Remove element from list, decrement list size
+                self.pkt.pop(self.size - num)
+                self.size -= 1
 
-            # Set values for next thread to run
-            if self.flag == self.max_num - 1:
-                self.flag == 1
-            else:
-                self.flag += 1
+                # Set values for next thread to run
+                if self.flag == self.max_num:
+                    self.flag = 1
+                else:
+                    self.flag += 1
 
-            # Release lock, wake up threads
-            self.cond.notify_all()
-            self.lock.release()
+                print("Thread Num: {} Max Num: {} List Size: {} Flag: {}".format(num, self.max_num, self.size, self.flag))
+
+                # Release lock, wake up threads
+                self.cond.notify_all()
+                self.lock.release()
 
 
     '''
@@ -79,7 +87,7 @@ class Sniffer:
         to listen for and sort incoming packets.
     '''
     def createThread(self):
-        thread_array = [Thread(target = self.filter, args = (i, )) for i in range(1, self.max_num)]
+        thread_array = [Thread(target = self.filter, args = (i, )) for i in range(1, self.max_num + 1)]
 
         for thread in thread_array:
             thread.start()

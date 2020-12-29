e77297815bd7c5dfca0d24143dcbcdda0e1c5f9d
rmhowe425 <45905457+rmhowe425@users.noreply.github.com>
2020-12-28 23:29:46 -0500

    Add files via upload

--- Sniffer.py
+++ Sniffer.py
@@ -1,4 +1,6 @@
 from pcap import pcap
+from Src.AccessPoint import AccessPoint
+from threading import Thread, Lock, Condition
 from Src.radtiotap.radiotap import radiotap_parse, ieee80211_parse
 
 class Sniffer:
@@ -8,23 +10,54 @@ class Sniffer:
         @param name: Name of adapter / file to read packets from.
     '''
     def __init__(self, name = None):
+        self.size = 1
+        self.lock = Lock()
+        self.cond = Condition(lock = self.lock)
+        self.ap_list = dict()
+        self.client_list = dict()
         self.sniff = pcap(name = name, promisc = True, immediate = True, timeout_ms = 50)
+
+    '''
+        Filter out and sort packets as either a client or access point.
+        @param pk: Packet to be inspected
+    '''
+    def filter(self, pk):
+        offset, rt_data = radiotap_parse(pk)
+
+        # Broadcast packet
+        if pk[offset] == 0x80:
+            iEE_offset, ap_info = ieee80211_parse(pk, offset)
+            start = iEE_offset + 14
+
+            # SSID
+            ssid = (pk[start: start + pk[iEE_offset + 13]]).decode()
+            # Power
+            pwr = rt_data['dbm_antsignal']
+            # MAC address
+            mac = ap_info['addr3']
+
+            # Add new item to dictionary
+            self.lock.acquire()
+            self.ap_list[ssid] = AccessPoint(pwr, mac, ssid)
+            self.lock.release()
 
     '''
         Records an unlimited number of packets.
     '''
     def sniffPacket(self):
-
-        num = 1
+        thread_array = []
 
         try:
             pkt = self.sniff.readpkts()
-            num = 1
 
             for pk in pkt:
-                offset = radiotap_parse(pk[1])[0]
-                print("{})  {}".format(num, ieee80211_parse(pk[1], offset)))
-                num += 1
+                t = Thread(target = self.filter, args = (pk[1], ))
+                t.start()
+                thread_array.append(t)
+                self.size += 1
+                
         except Exception as e:
             print("Error Sniffing packets\n{}".format(str(e)))
 
+        for t in thread_array:
+            t.join()
--- exec.py
+++ exec.py
@@ -1,7 +1,10 @@
+from time import time
 from Src.Sniffer import Sniffer
 
 def main():
+    start = time()
     packet = Sniffer("../Artifacts/LargePacketCapture.pcapng")
     packet.sniffPacket()
 
+    print("Elapsed time: {}".format(round(time() - start, 2)))
 main()
